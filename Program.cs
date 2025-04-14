4e665345c1f63f8e54cca77bc182cb1c11deeaa4
Evika <evelinashubina389@gmail.com>
2025-04-14 16:18:50 +0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace ProjectFileTasks
+{
+    class Program
+    {
+        static void Main()
+        {
+            bool exitFlag;
+            TaskSelection taskSelection = new TaskSelection();
+            taskSelection.inputNumberTask(out exitFlag);
+
+            if (exitFlag)
+            {
+                Console.WriteLine("Выход из программы.");
+            }
+        }
+    }
+}
---
+++ Toy.cs
@@ -0,0 +1,14 @@
+﻿namespace ProjectFileTasks
+{
+    /// <summary>
+    /// Игрушка с атрибутами
+    /// </summary>
+    [Serializable]
+    public class Toy
+    {
+        public string Name { get; set; }
+        public int Price { get; set; }
+        public int MinAge { get; set; }
+        public int MaxAge { get; set; }
+    }
+}
